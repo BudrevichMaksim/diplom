d8af3e6f396bbf6f6226b171eefa00efd7236825
Maksim Budrevich <maksimbudrevich@gmail.com>
2026-04-30 11:04:09 +0300

    feat(bot.py): add basic bot functionality

---
+++ bot/bot.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN: str = str(getenv("BOT_TOKEN"))
+
+START_MESSAGE: str = "Я создан помочь понять, общается ли с тобой реальный человек, перешли голосовое и я отвечу в ближайшее время!"
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(START_MESSAGE)
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
