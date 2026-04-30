309efbf0aa1d92c4ec123dea03fe03bcbfbc650b
Maksim Budrevich <maksimbudrevich@gmail.com>
2026-04-30 11:32:03 +0300

    feat(bot.py): add voice messages handler

--- bot/bot.py
+++ bot/bot.py
@@ -2,17 +2,18 @@ import asyncio
 import logging
 import sys
 from os import getenv
+from pathlib import Path
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
+
 
 TOKEN: str = str(getenv("BOT_TOKEN"))
-
 START_MESSAGE: str = "Я создан помочь понять, общается ли с тобой реальный человек, перешли голосовое и я отвечу в ближайшее время!"
-
+REPLY_TO_VOICE: str = "Сообщение отправлено на анализ, ожидайте..."
 
 dp = Dispatcher()
 
@@ -23,6 +24,23 @@ async def command_start_handler(message: Message) -> None:
     This handler receives messages with `/start` command
     """
     await message.answer(START_MESSAGE)
+
+
+@dp.message(F.voice)
+async def voice_message_handler(message: Message, bot: Bot):
+    """
+    This handler recieves voice message
+    """
+    assert message.voice is not None
+
+    destination_dir = Path("downloads/voices")
+    destination_dir.mkdir(parents=True, exist_ok=True) 
+    
+    file_path = destination_dir / f"{message.voice.file_id}.ogg"
+    
+    await bot.download(message.voice, destination=file_path)
+
+    await message.reply(REPLY_TO_VOICE)
 
 
 async def main() -> None:
