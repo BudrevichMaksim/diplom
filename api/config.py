6468982762f3313adb06c52685292a8440e7c68d
Maksim Budrevich <maksimbudrevich@gmail.com>
2026-05-13 19:54:58 +0300

    feat(api/config.py): add BaseSettings class

--- api/config.py
+++ api/config.py
@@ -1,4 +1,22 @@
-from os import getenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
 
-EXTRACTOR:str = getenv("EXTRACTOR","mel")
-DETECTOR: str = getenv("DETECTOR","cnn")
+
+class Settings(BaseSettings):
+    """
+    Application settings and environment configuration.
+    Defined using Pydantic for automatic validation and type hinting.
+    """
+
+    # We can restrict the values to only what our API supports
+    EXTRACTOR: Literal["mel"] = "mel"
+    DETECTOR: str = "rcnn"
+
+    # Configuration for the Settings class itself
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+# Create a singleton instance to be used across the app
+settings = Settings()
